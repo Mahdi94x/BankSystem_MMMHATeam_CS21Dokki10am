148c556bbac25be194485469fd58b819ba869be5
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2024-08-23 03:56:08 +0300

    ValidationPersonTemplate23Aug

--- BankSystem_MMMHATeam_CS21Dokki10am/include/Person.h
+++ BankSystem_MMMHATeam_CS21Dokki10am/include/Person.h
@@ -1,15 +1,39 @@
 #ifndef PERSON_H
 #define PERSON_H
-
+#include <iostream>
+using namespace std;
+#include "Validation.h"
 
 class Person
 {
-    public:
+protected:
+    // Attributes:
+    int id;
+    string name, password;
 
+public:
+    //Constructors:
+    Person() {}
 
-    protected:
+    Person(int id, string name, string password) {}
 
-    private:
+    // Setters:
+    void setID(int id) {}
+    void setName(string name) {}
+
+    void setPassword(string password) {}
+
+    //Getters:
+    int getID() {}
+
+    string getName() {}
+
+    string getPassword() {}
+
+    //Methods:
+	// Abstracted Pure Virtual Method
+	virtual void display() = 0;
+
 };
 
 #endif // PERSON_H
--- BankSystem_MMMHATeam_CS21Dokki10am/include/Validation.h
+++ BankSystem_MMMHATeam_CS21Dokki10am/include/Validation.h
@@ -1,14 +1,21 @@
 #ifndef VALIDATION_H
 #define VALIDATION_H
+#include <iostream>
+using namespace std;
+#include <regex>
+#include <string>
 
 
 class Validation
 {
-    public:
+public:
+    static bool validateName(string name) {}
+    static bool validatePassword(string password) {}
+    static bool validateBalance(double balance) {}
+    static bool validateSalary(double salary) {}
+    static bool validateAmount(double amount) {}
 
-    protected:
 
-    private:
 };
 
 #endif // VALIDATION_H
