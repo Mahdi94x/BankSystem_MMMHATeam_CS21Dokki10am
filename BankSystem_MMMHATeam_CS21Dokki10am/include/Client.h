7857fb3e300af691f639a3c6a8590452dd531bdd
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2024-08-29 22:41:44 +0300

    Helal_ClientClass_Adding_setBalance

--- BankSystem_MMMHATeam_CS21Dokki10am/include/Client.h
+++ BankSystem_MMMHATeam_CS21Dokki10am/include/Client.h
@@ -29,6 +29,29 @@ public:
         countClients++;
 }
 
+    //Setters: // Helal
+    void setBalance(double balance) {
+        int attempt{};
+        const int maxattempts{ 3 };
+        while (!(Validation::validateBalance(balance)) && attempt < maxattempts) {
+            cout << "Apologies! Invalid Balance. The Balance must be equal to or higher than 1500.\n";
+            attempt++;
+            cout << "Attemp => (" << attempt << "/" << maxattempts << ")" << " Enter the modified balance : ";
+            cin >> balance;
+            // Clear the input buffer to avoid issues with getline or other input methods.
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        if (Validation::validateBalance(balance)) {
+            this->balance = balance;
+        }
+        else {
+            cout << "Max Attemps have been reached! => Setting balance to default value\n";
+            cout << "Kindly Contact our support ASAP\n";
+            this->balance = 0;
+        }
+}
+
     // Methods:
     void withdraw(double amount) {}
 
