c2f47f0b3b6fd071c73d5ecd4099385e4ac94015
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2024-09-08 23:16:51 +0300

    Mahdi_Phase2_ParserClass_AddingSplitFunction
    the split function is used when getting the data from the txt files in one data bulk it used a 2D vector to split the data bulk into single lines attributes and then split the single line into 5 different attributes

--- BankSystem_MMMHATeam_CS21Dokki10am/include/Parser.h
+++ BankSystem_MMMHATeam_CS21Dokki10am/include/Parser.h
@@ -1,16 +1,36 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-
 class Parser
 {
     public:
-        Parser() {}
-        virtual ~Parser() {}
+	static vector<vector<string>> split(string dataBulk) {
+		vector<vector<string>> allData;
+		string line;
+		stringstream ss(dataBulk);
 
-    protected:
+		// Split the bulk data into individual lines
+		while (getline(ss, line, '\n')) {
+			vector<string> singleLineAttributes;
+			string token;
+			stringstream lineStream(line);
 
-    private:
+			// Split each line into attributes based on the '#' delimiter
+			while (getline(lineStream, token, '~')) {
+				singleLineAttributes.push_back(token);
+			}
+
+			// Add the vector of attributes to the main vector
+			singleLineAttributes.shrink_to_fit();
+			allData.push_back(singleLineAttributes);
+
+		}
+		return allData;
+	}
+
+	static Client parseToClient(vector<string> singleLineAttributes) {}
+	static Employee parseToEmployee(vector<string> singleLineAttributes) {}
+	static Admin parseToAdmin(vector<string> singleLineAttributes) {}
 };
 
 #endif // PARSER_H
