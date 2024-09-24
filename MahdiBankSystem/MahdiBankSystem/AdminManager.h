977b2b07bcbce8623a5c31a9b71704814a78047e
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2024-09-25 01:01:40 +0300

    AddedEmployeeManagerAndAdminManagerLogic

--- MahdiBankSystem/MahdiBankSystem/AdminManager.h
+++ MahdiBankSystem/MahdiBankSystem/AdminManager.h
@@ -8,86 +8,146 @@ class AdminManager
 {
 public:
 	static int printAdminMenu() { // Amira
-		
+		cout << "Please choose one of the following options:" << endl;
+		cout << "1.  Check Salary" << endl;
+		cout << "2.  Display Information" << endl;
+		cout << "3.  Update Password" << endl;
+		cout << "4.  Add Client" << endl;
+		cout << "5.  List All Clients" << endl;
+		cout << "6.  Search and Display Client" << endl;
+		cout << "7.  Edit Client Info" << endl;
+		cout << "8.  Reactivate Client" << endl;
+		cout << "9.  Add Employee" << endl;
+		cout << "10. List All Employees" << endl;
+		cout << "11. Search and Display Employee" << endl;
+		cout << "12. Edit Employee Info" << endl;
+		cout << "13. Reactivate Employee" << endl;
+		cout << "14. Exit" << endl;
+
+		int choice;
+		cout << "Please, Choose an Option: ";
+		cin >> choice;
+		return choice;
 	}
 
 	static void newEmployee(Admin* a) { // Amira
-		
+		a->addEmployee();
 	}
 
 	static void listAllEmployees(Admin* a) { // Amira
-		
+		a->listEmployee();
 	}
 
 	static void searchForEmployee(Admin* a) { // Hadeer
-		
+		int id;
+		cout << "Enter the ID of the Employee you want to search for: ";
+		cin >> id;
+		if (a->searchEmployee(id)) {
+			cout << "Employee with ID: " << id << " is found!" << endl;
+			a->displayEmployee(id);
+		}
 	}
 
 	static void editEmployeeInfo(Admin* a) { // Hadeer
-		
+		int id;
+		cout << "Enter the ID of the employee you want to edit: ";
+		cin >> id;
+		if (a->searchEmployee(id)) {
+			cout << "Employee's ID: " << id << " is Found." << endl;
+			string name;
+			cout << "Enter the name of the employee: ";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			getline(cin, name);
+			cout << "Enter the password of the employee: ";
+			string password;
+			cin >> password;
+			cout << "Enter the phone number of the employee: ";
+			string phoneNumber;
+			cin >> phoneNumber;
+			cout << "Enter the salary of the employee: ";
+			double salary;
+			cin >> salary;
+			a->editEmployee(id, name, password, phoneNumber, salary);
+		}
 	}
 
 	static Admin* login(int id, string password) { // Hadeer
-		
+		for (Admin& a : adminsVector) {
+			if (a.getID() == id && a.getPassword() == password) {
+				return &a;
+			}
+		}
+		cout << "Invalid ID or Password!" << endl;
+		return nullptr;
 	}
 
 	static bool adminOptions(Admin* a, int choice) {
 
 		switch (choice) {
 		case 1: // Mohammed
-			
+			a->checkSalary();
 			break;
-		case 2: // Mohammed
 
+		case 2: // Mohammed
+			a->display();
 			break;
 
 		case 3: // Mohammed
-
+			ClientManager::updatePassword(a);
 			break;
 
 		case 4: // Mohammed
-
+			EmployeeManager::newClient(a);
 			break;
 
 		case 5: // Mohammed
-
+			EmployeeManager::listAllClients(a);
 			break;
 
 		case 6: // Mohammed
-
+			EmployeeManager::searchForClient(a);
 			break;
 
 		case 7: // Mohammed
-
+			EmployeeManager::editClientInfo(a);
 			break;
 
 		case 8: //Helal
-			
+			int idc;
+			cout << "Enter the ID of the client you want to reactivate: ";
+			cin >> idc;
+			a->reactivateClient(idc);
 			break;
 
 		case 9: //Helal
-
+			newEmployee(a);
 			break;
 
 		case 10: //Helal
-
+			listAllEmployees(a);
 			break;
 
 		case 11: //Helal
-
+			searchForEmployee(a);
 			break;
 
 		case 12: //Mostafa
-
+			editEmployeeInfo(a);
 			break;
 
 		case 13: //Mostafa
-			
+			int ide;
+			cout << "Enter the ID of the employee you want to reactivate: ";
+			cin >> ide;
+			a->reactivateEmployee(ide);
 			break;
 
 		case 14: //Mostafa
+			return false;
 
 		default: //Mostafa
+			cout << "Invalid choice! Please choose a number between 1 and 14." << endl;
 		}
 		return true;
 	}
--- MahdiBankSystem/MahdiBankSystem/EmployeeManager.h
+++ MahdiBankSystem/MahdiBankSystem/EmployeeManager.h
@@ -5,64 +5,119 @@
 class EmployeeManager
 {
 public:
-	static void printEmployeeMenu(){ // Mohammed
+	static int printEmployeeMenu(){ // Mohammed
+		cout << "Please choose one of the following options:" << endl;
+		cout << "1.  Check Salary" << endl;
+		cout << "2.  Display Information" << endl;
+		cout << "3.  Update Password" << endl;
+		cout << "4.  Add Client" << endl;
+		cout << "5.  List All Clients" << endl;
+		cout << "6.  Search and Display Client" << endl;
+		cout << "7.  Edit Client Info" << endl;
+		cout << "8.  Reactivate Client" << endl;
+		cout << "9.  Exit" << endl;
+
+		int choice;
+		cout << "Please, Choose an Option: ";
+		cin >> choice;
+		return choice;
 	}
 
 	static void newClient(Employee* e){ //Mohammed
+		e->addClient();
 	}
 
 	static void listAllClients(Employee* e){ // Mohammed
+		e->listClient();
 	}
 
 	static void searchForClient(Employee* e){ // Mohammed
+		int id;
+		cout << "Enter the ID of the client you want to search for: ";
+		cin >> id;
+		if (e->searchClient(id)) {
+			cout << "Client's ID: " << id << " is Found." << endl;
+			e->displayClient(id);
+		}
 	}
 
 	static void editClientInfo(Employee* e){ // Helal
+		int id;
+		cout << "Enter the ID of the client you want to edit: ";
+		cin >> id;
+		if (e->searchClient(id)) {
+			cout << "Client's ID: " << id << " is Found." << endl;
+			string name;
+			cout << "Enter the name of the client: ";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			getline(cin, name);
+			cout << "Enter the password of the client: ";
+			string password;
+			cin >> password;
+			cout << "Enter the phone number of the client: ";
+			string phoneNumber;
+			cin >> phoneNumber;
+			cout << "Enter the balance of the client: ";
+			double balance;
+			cin >> balance;
+			e->editClient(id, name, password, phoneNumber, balance);
+		}
 	}
 
 	static Employee* login (int id, string password){ // Mostafa
+		for (Employee& e : employeesVector) {
+			if (e.getID() == id && e.getPassword() == password) {
+				return &e;
+			}
+		}
+		cout << "Invalid ID or Password!" << endl;
+		return nullptr;
 	}
 
 	static bool employeeOptions(Employee* e, int choice) {
 
 		switch (choice) {
 		case 1: // Mostafa
-			
+			e->checkSalary();
 			break;
 
 		case 2: // Mostafa
-			
+			e->display();
 			break;
 
 		case 3: // Mostafa
-			
+			ClientManager::updatePassword(e);
 			break;
 
 		case 4: //Hadeer
-			
+			newClient(e);
 			break;
 
 		case 5: //Hadeer
-			
+			listAllClients(e);
 			break;
 
 		case 6: //Hadeer
-			
+			searchForClient(e);
 			break;
 
 		case 7: // Amira
-			
+			editClientInfo(e);
 			break;
 
 		case 8: // Amira
-			
+			int id1;
+			cout << "Enter the ID of the client you want to reactivate: ";
+			cin >> id1;
+			e->reactivateClient(id1);
 			break;
 
 		case 9: // Amira
-			
+			return false;
 
 		default: // Amira
-			
+			cout << "Invalid choice! Please choose a number between 1 and 9." << endl;
 		}
 		return true;
 	}
